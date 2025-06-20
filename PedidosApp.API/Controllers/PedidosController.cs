627f4cc9e5e3c39053ee14bd0cf0d073db151217
sergiosistemapeoplenet <sergio@sistemapeoplenet.com.br>
2025-06-20 20:01:22 -0300

    fix: Inclusão de etapa de testes no workflow e mapeamento dos endpoints da API.

--- PedidosApp.API/Controllers/PedidosController.cs
+++ PedidosApp.API/Controllers/PedidosController.cs
@@ -13,20 +13,26 @@ namespace PedidosApp.API.Controllers
             return StatusCode(501); //Not Implemented
         }
 
-        [HttpPut]
-        public async Task<IActionResult> Put()
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(Guid id)
         {
             return StatusCode(501); //Not Implemented
         }
 
-        [HttpDelete]
-        public async Task<IActionResult> Delete()
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(Guid id)
         {
             return StatusCode(501); //Not Implemented
         }
 
         [HttpGet]
         public async Task<IActionResult> Get()
+        {
+            return StatusCode(501); //Not Implemented
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(Guid id)
         {
             return StatusCode(501); //Not Implemented
         }
